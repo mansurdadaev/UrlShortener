20c6d6eb0af91248e9f9f4a0bb599f0aa991d693
Mansur <368016Mans>
2022-06-25 09:27:59 +0300

    Initial commit

---
+++ src/test/java/com/yourcodereview/mansur/task2/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourcodereview.mansur.task2.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
